4c25cd0e070b94cb8a4c5cf27832a0f3573fdc04
YOHK YOHK <duckdev84@gmail.com>
2022-05-13 18:34:40 +0700

    move to my pc

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -24,7 +24,7 @@ export function Root(): JSX.Element {
   }, [h1Month]);
 
   return (
-    <div className="flex items-center justify-center py-8 px-4">
+    <div className="">
       <div className="max-w-sm w-full shadow-lg">
         <div className="md:p-8 p-5 dark:bg-gray-800 bg-white rounded-t">
           <div className="px-4 flex items-center justify-between">
--- src/types/RtwDatepickBase.ts
+++ src/types/RtwDatepickBase.ts
@@ -1,10 +1,14 @@
 import {MonthChangeEventHandler} from "./EventHandlers";
 
+export interface ModifierClassName {
+  l1: string;
+}
+
 export interface RtwDatepickBase {
   className?: string;
   year: number;
   month: number;
   date: number;
-  config?: string;
   onMonthChange?: MonthChangeEventHandler;
+  classNames?: ModifierClassName;
 }
